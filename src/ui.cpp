0596dabaf89f22c1a16bfe5c9f3b6d701b6a14ba
rootElmo <elmo.rohula@gmail.com>
2021-03-02 18:20:15 +0200

    Remove commented code

--- src/ui.cpp
+++ src/ui.cpp
@@ -28,54 +28,7 @@ void ui::onEncIncr() {
     if (print_incr != 0) {
         my_seq.incrTempo(print_incr);
     }
-    /*
-    if (digitalRead(ENC_BTN) == LOW) { // If encoder button is pressed, set note value
-        note_list[note_selected] += print_incr; // move this whole shebang to buttons.cpp,
-        Serial.print("Note value: ");           // since the encoder button IS A BUTTON
-        Serial.println(note_list[note_selected]);
-    } else if (digitalRead(ENC_BTN) == HIGH) { // if encoder button is not pressed, set selected note
-        note_selected += print_incr;
-        if (note_selected >= 16) {
-            note_selected = 0;
-        }
-        if (note_selected <= -1) {
-            note_selected = 15;
-        }
-        Serial.print("Note selected: ");
-        Serial.println(note_selected);
-    }
-    */
 }
-
-/*
-Printing of notes is not necessary anymore for debugging.
-Need to completely remove this and related parts from ui.cpp
-void ui::printNotes() { // print note_list notes
-    if (digitalRead(PRINT_BTN) == HIGH) {
-        if (btn_state) {
-            btn_state = false;
-        }
-        return;
-    }
-    if (digitalRead(PRINT_BTN) == LOW) {
-        if (btn_state) {
-            return;
-        }
-        btn_state = true;
-        Serial.println("Pattern notes:"); // Same logic could be used in seq.cpp later
-        for (int i = 0; i <= 15; i++) { // "i" could be defined by something like "MAX_STEPS" in the future
-            Serial.print("[");          // to only scan notes defined by pattern length
-            Serial.print(note_list[i]);
-            if (i == 15) {
-                Serial.println("]");
-                return;
-            }
-            Serial.print("], ");
-        }
-    }
-}
-
-*/
 
 void ui::onPlayBtn() {
     my_seq.onPlay();
